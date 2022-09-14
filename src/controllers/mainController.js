5e3788e8895a47f543e515e308e06cd2ca33d359
matias <matias.alvarellos@gmail.com>
2022-09-14 01:25:24 -0300

    ajuste mainController

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,7 +2,7 @@
 // un objeto literal con las acciones para cada ruta
 const mainController = {
     home: function(req, res){
-        res.render("product-detail");
+        res.render("index");
     }
 }   
 // Acá exportamos el objeto mainController
